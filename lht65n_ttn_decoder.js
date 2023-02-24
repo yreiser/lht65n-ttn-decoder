176094e397eeed87cdf5761c14df3ee275479b31
Yves Reiser <develop@yvesreiser.lu>
2023-02-24 18:52:46 +0100

    New payload format

--- lht65n_ttn_decoder.js
+++ lht65n_ttn_decoder.js
@@ -1,50 +1,69 @@
 function decodeUplink(input) {
-  var data = {};
   let external = false; // set to true if external temp. sensor connected
-  
+  var extTemp = 0;
   // ------------- BATTERY ---------------
   let batBytes = input.bytes[0] << 8 | input.bytes[1];
   let batStateValue = batBytes >> 14; // get first 2 bits of 2 battery bytes
   var batStateMsg = "";
   switch(batStateValue) {
     case 0:
-      data.batState = "Ultra Low";
+      batStateMsg = "Ultra Low";
       break;
     case 1:
-      data.batState = "Low";
+      batStateMsg = "Low";
       break;
     case 2:
-      data.batState = "OK";
+      batStateMsg = "OK";
       break;
     case 3:
-      data.batState = "Good";
+      batStateMsg = "Good";
       break;
 } 
-  data.batVoltage_V = (batBytes & 0x3FFF)/ 1000; // get last 14 bit of 2 bytes
+  let batV = (batBytes & 0x3FFF)/ 1000; // get last 14 bit of 2 bytes
   
   // ------------- INTERNAL TEMP ---------------
   let intTempBytes = input.bytes[2] << 8 | input.bytes[3];
   let intNegFlag = (intTempBytes>>15)*(-65536);
-  data.intTemp_C=(intNegFlag+(intTempBytes)) / 100;
+  let intTemp=(intNegFlag+(intTempBytes)) / 100;
 
   // ------------- INTERNAL HUMIDITY ---------------
 
   let intHumBytes = input.bytes[4] << 8 | input.bytes[5];
-  data.intHum_pct = intHumBytes / 10;
+  let intHum = intHumBytes / 10;
   
   // ------------- EXTERNAL TEMP. ---------------
   
-  let extSensorByte = input.bytes[6];
-  let extTempBytes = input.bytes[7] << 8 | input.bytes[8];
-  if (extSensorByte == 1 && external){
-  let extNegFlag = (extTempBytes>>15)*(-65536);
-  data.extTemp=(extNegFlag+(extTempBytes)) / 100;
+  if(external){
+    let extSensorByte = input.bytes[6];
+    let extTempBytes = input.bytes[7] << 8 | input.bytes[8];
+    if (extSensorByte == 1 && external){
+      let extNegFlag = (extTempBytes>>15)*(-65536);
+      extTemp=(extNegFlag+(extTempBytes)) / 100;
+    }
   }
   
   // ------------- OUTPUT ---------------
-  return {
-    data: {
-      data
-    }
-  };
-}
+  
+  if(!external){
+    return {
+      data: {
+        batState: batStateMsg,
+        batV_V: batV,
+        intTemp_C: intTemp,
+        intHum_pct: intHum,
+      }
+    };
+  }
+  
+  else{
+    return {
+      data: {
+        batState: batStateMsg,
+        batV_V: batV,
+        intTemp_C: intTemp,
+        intHum_pct: intHum,
+        extTemp_C: extTemp
+      }
+    };
+  }
+  }
